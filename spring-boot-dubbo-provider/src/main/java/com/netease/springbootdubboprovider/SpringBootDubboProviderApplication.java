ce4c8b1c69b8f44cce6c64a3491c1b8023b72f55
hzbinbinli <libinbin@corp.netease.com>
2019-10-18 14:03:13 +0800

    update

---
+++ spring-boot-dubbo-provider/src/main/java/com/netease/springbootdubboprovider/SpringBootDubboProviderApplication.java
@@ -0,0 +1,14 @@
+package com.netease.springbootdubboprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAutoConfiguration
+public class SpringBootDubboProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDubboProviderApplication.class, args);
+    }
+
+}
